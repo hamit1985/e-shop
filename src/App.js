16fa2a16100aa199b24e6ca4fa2957ec0a8b9126
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2022-12-28 10:36:37 +0300

    adding sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-
 const App =() => {
 
    const categories = [
@@ -28,8 +27,8 @@ const App =() => {
 
   return (
     <div className="categories-container">
-      {categories.map(({title}) => (
-      <div className="category-container">
+      {categories.map(({title, id}) => (
+      <div key={id} className="category-container">
         <div className="background-image" />
         <div className="category-body-container">
           <h2>{title}</h2>
