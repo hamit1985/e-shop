a0ca5fb885fbb4dee7551bc289e89f56da5040f8
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2022-12-28 10:20:57 +0300

    setting up categories

--- src/App.js
+++ src/App.js
@@ -1,44 +1,42 @@
 
 
 const App =() => {
+
+   const categories = [
+     {
+      id: 1,
+      title: 'Hats'
+     },
+     {
+      id: 2,
+      title: 'Jackets'
+     },
+     {
+      id: 3,
+      title: 'Sneakers'
+     },
+     {
+      id: 4,
+      title: 'Womens'
+     },
+     {
+      id: 5,
+      title: 'Mans'
+     },
+   ]
+
+
   return (
     <div className="categories-container">
+      {categories.map(({title}) => (
       <div className="category-container">
-        {   }
+        <div className="background-image" />
         <div className="category-body-container">
-          <h2>Hats</h2>
+          <h2>{title}</h2>
           <p>shop now</p>
         </div>
       </div>
-      <div className="category-container">
-        {   }
-        <div className="category-body-container">
-          <h2>jackets</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {   }
-        <div className="category-body-container">
-          <h2>sneakers</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {   }
-        <div className="category-body-container">
-          <h2>womens</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {   }
-        <div className="category-body-container">
-          <h2>mans</h2>
-          <p>shop now</p>
-        </div>
-      </div>
-      
+      ))}
     </div>
   );
 }
