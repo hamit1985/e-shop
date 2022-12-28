d652c5e4c90822778238b3a01745974ec99ab1e3
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2022-12-28 10:04:12 +0300

    75. Scaffolding Our Capstone Project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 
-import './App.css';
 
-function App() {
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        {   }
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {   }
+        <div className="category-body-container">
+          <h2>jackets</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {   }
+        <div className="category-body-container">
+          <h2>sneakers</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {   }
+        <div className="category-body-container">
+          <h2>womens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        {   }
+        <div className="category-body-container">
+          <h2>mans</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
