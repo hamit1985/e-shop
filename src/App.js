befebc32ff4af150521af9670a7bc38c95708072
hamit1985 <111700185+hamit1985@users.noreply.github.com>
2023-01-02 10:59:10 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1> I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 const Shop = () => {
   return (
    <h1>I am the shop page</h1>
@@ -11,7 +22,8 @@ const Shop = () => {
 const App =() => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element= {<Navigation />}>
+        <Route index={true} element={<Home />}/>
         <Route path="shop" element={<Shop />}/>
       </Route>
     </Routes>
